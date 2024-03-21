1efd6d737de396caf799aba1959d331e3ef6affe
saracbza <sasa.cb2002@gmail.com>
2024-03-21 01:12:10 -0300

    Adicionando rotas e funções faltantes do crud (encontrar específico, deletar e editar)

--- src/controllers/task/task.controller.ts
+++ src/controllers/task/task.controller.ts
@@ -24,4 +24,61 @@ export default class TaskController {
 
         return res.status(200).json(tasks)
     }
+
+    static async show (req: Request, res: Response){
+        const { id } = req.params 
+
+        if (!id || isNaN(Number(id))) 
+	        return res.status(400).json({erro: 'O id é obrigatório'})
+    
+        const task = await Task.findOneBy({id: Number(id)})
+        
+        if (!task) 
+	        return res.status(404)
+
+        return res.json(task)    
+    }
+
+    static async delete (req: Request, res: Response){
+
+        const { id } = req.params 
+
+        if (!id || isNaN(Number(id)))
+            return res.status(400).json({erro: 'O id é obrigatório'})
+    
+        const task = await Task.findOneBy({id: Number(id)})
+        
+        if (!task)
+        return res.status(404).json({erro: 'Não encontrado'})
+
+        await task.remove()
+
+        return res.status(204).send() 
+    }
+
+    static async update (req: Request, res: Response){
+
+        const { id } = req.params 
+        
+        const { title, completed } = req.body
+
+        if (!id || isNaN(Number(id)))
+            return res.status(400).json({erro: 'O id é obrigatório'})
+
+        if (completed === undefined)
+            return res.status(400).json({ error: 'O completado é obrigatório' })
+    
+        const task = await Task.findOneBy({id: Number(id)})
+        
+        if (!task)
+            return res.status(400).json({erro: 'Não encontrado'})
+
+        task.title = title ?? task.title 
+
+        task.title = title
+        task.completed = completed
+        await task.save()                            
+        
+        return res.status(204).send() 
+    }
 }
