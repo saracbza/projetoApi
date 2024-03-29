3182a3a911c85bc84caef115a375fd5886cc4eea
saracbza <sasa.cb2002@gmail.com>
2024-03-29 11:32:36 -0300

    Criando funcao para login com futura validacao por token e add sua rota

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
 import User from '../../models/user.entity'
+import Token from '../../models/token.entity'
 
 export default class AuthController{
     static async store (req: Request, res: Response){
@@ -21,5 +22,37 @@ export default class AuthController{
             name: user.name,
             email: user.email
         }) 
+    }
+
+    static async login (req: Request, res: Response){
+        const {email, password} = req.body
+
+        if (!email || !password) return res.status(400).json({error: "Email e senha são obrigatórios"})
+
+        const user = await User.findOneBy ({email})
+        if (!user) return res.status(401).json({error: "Usuário não encontrado"})
+
+        const passwCheck = bcrypt.compareSync(password, user.password)
+        if (!passwCheck) return res.status(401).json({error: "Senha inválida"})
+
+        await Token.delete({user: {id: user.id}})
+
+        const token = new Token()
+        
+        const stringRand = user.id + new Date().toString() 
+        token.token = bcrypt.hashSync(stringRand, 1).slice(-20)
+
+        token.expiredAt = new Date (Date.now()+60*60*1000) //expirar token em 1 hora
+
+        token.refreshToken = bcrypt.hashSync(stringRand+2, 1).slice(-20)
+
+        token.user = user
+        await token.save()
+
+        return res.json({
+            token: token.token,
+            expiredAt: token.expiredAt,
+            refreshToken: token.refreshToken
+        })
     }
 }
--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -4,5 +4,6 @@ import AuthController from '../../controllers/auth/auth.controller'
 const authRoutes = Router()
 
 authRoutes.post('/register', AuthController.store)
+authRoutes.post('/login', AuthController.login)
 
 export default authRoutes
