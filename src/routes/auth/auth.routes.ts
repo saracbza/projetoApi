51ba40e29ede9b7d2e37fcf650183703f81ce21c
Sara Barboza <132144076+saracbza@users.noreply.github.com>
2024-05-18 15:44:47 -0300

    correcao auth.routes.ts

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -6,6 +6,6 @@ const authRoutes = Router()
 authRoutes.post('/register', AuthController.store)
 authRoutes.post('/login', AuthController.login)
 authRoutes.post('/refresh', AuthController.refresh)
-authRoutes.post('/logout', AuthController.logou
+authRoutes.post('/logout', AuthController.logout)
 
 export default authRoutes
